23c270dd9d11d093ee90cfdd9c91dc98cfdd647e
Sigmar Stefansson <sigmarkarl@hotmail.com>
2024-10-01 08:51:34 +0000

    our changes

--- jupyterlab/semver.py
+++ jupyterlab/semver.py
@@ -747,6 +747,13 @@ def make_range(range_, loose):
 
 class Range:
     def __init__(self, range_, loose):
+        if "/services" in range_:
+            range_ = "~7.3.0-beta.1"
+        elif "/ui-components" in range_:
+            range_ = "~4.3.0-beta.1"
+        elif "/mainmenu-extension" in range_:
+            range_ = "~4.3.0-beta.1"
+        
         self.loose = loose
         #  First, split based on boolean or ||
         self.raw = range_
